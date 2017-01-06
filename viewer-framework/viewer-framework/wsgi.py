270740b8915601f7abbdb1db0f156ff184776044
Kristof <stuff@kritten.org>
2017-01-06 15:44:53 +0100

    *restructured project

--- viewer-framework/viewer-framework/wsgi.py
+++ viewer-framework/viewer-framework/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for viewer project.
+WSGI config for viewer-framework project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "viewer.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "viewer-framework.settings")
 
 application = get_wsgi_application()
