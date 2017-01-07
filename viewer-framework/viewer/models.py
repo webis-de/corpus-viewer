6384aa209a8899d80ffddd0109999892e9d1442c
Kristof Komlossy <Kristof Komlossy>
2017-01-07 01:14:56 +0100

    +started html-page

--- viewer-framework/viewer/models.py
+++ viewer-framework/viewer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class m_Entity(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+class m_Tag(models.Model):
+    class Meta:
+        ordering = ['name']
+        
+    name = models.CharField(max_length=100, unique=True)
+    color = models.CharField(max_length=7, default="#000000")
+    m2m_entity = models.ManyToManyField(m_Entity)
+
+    def __str__(self):
+        return '<Tag [%s]>' % (self.name)
---
+++ viewer-framework/viewer/views/index.py
@@ -0,0 +1,6 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+def index(request):
+    context = {}
+    return render(request, 'viewer/index.html', context)
