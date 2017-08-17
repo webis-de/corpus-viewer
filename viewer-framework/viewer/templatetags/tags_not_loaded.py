769acb723a04fd2e321bb8aacbd077ea2863cc7a
Kristof Komlossy <stuff@kritten.org>
2017-08-17 14:51:23 +0200

    *formatted error log

--- viewer-framework/viewer/templatetags/tags_not_loaded.py
+++ viewer-framework/viewer/templatetags/tags_not_loaded.py
@@ -2,7 +2,14 @@ from django import template
 from viewer.classes.data.Manager_Data import Manager_Data 
 
 register = template.Library()
+from django.utils.safestring import mark_safe
+
 
 @register.simple_tag(takes_context=True)
 def check_is_loading(context):
     return context['state_loaded'] == Manager_Data.State_Loaded.LOADING
+
+@register.filter
+def prepare_for_html(value):
+	value = mark_safe("&nbsp;".join(value.split(' ')))
+	return value
