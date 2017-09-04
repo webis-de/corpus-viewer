696a4ff6329e69a519b26a3f83c709d6d0230968
Kristof Komlossy <kristof.komlossy@uni-weimar.de>
2017-09-04 09:38:19 +0200

    +web components support

---
+++ viewer-framework/viewer/views/view_item.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from viewer.views.shared_code import glob_manager_data, get_current_corpus
+import json
+
+def view_item(request, id_corpus, id_internal_item):
+    # id_corpus = get_current_corpus(request)
+    context = {}
+    print(id_corpus)
+    print(id_internal_item)
+    obj_item = glob_manager_data.get_item(id_corpus, int(id_internal_item))
+    print(obj_item)
+    # if request.method == 'POST':
+    #     input_secret_token = request.POST.get('secret_token')
+    #     request.session[id_corpus]['viewer__secret_token'] = input_secret_token
+    #     request.session.modified = True
+    #     return redirect('viewer:index')
+
+    context['json_item'] = json.dumps(obj_item)
+    context['id_corpus'] = id_corpus
+    return render(request, 'viewer/view_item.html', context)
