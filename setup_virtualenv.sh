e3c09429ab6e97a49373857ccb5f791038610adc
Kristof Komlossy <yiro4618@webis24.medien.uni-weimar.de>
2018-01-12 09:42:24 +0100

    *udpated setup_virtualenv.sh

--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -2,7 +2,7 @@
 # do necessary inital tasks
 
 mkdir venv_viewer
-virtualenv -p python3 venv_viewer/
+python3 -m venv venv_viewer/
 
 venv_viewer/bin/pip install django==1.10.6
 venv_viewer/bin/pip install whoosh==2.7.4
