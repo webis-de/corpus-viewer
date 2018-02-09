ff906b03cb4f8c572b9628a76f39b55680a541a6
Kristof Komlossy <yiro4618@webis24.medien.uni-weimar.de>
2018-02-09 17:06:42 +0100

    *improved update process

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # do necessary inital tasks
 
+git pull
 git submodule update --recursive --remote --init
 
 pip3 install -r requirements.txt
 
 cd ./viewer-framework
 python3 manage.py createcachetable
-python3 manage.py makemigrations viewer example_app
+# python3 manage.py makemigrations viewer example_app
 python3 manage.py migrate
