080238e31fab802ef333439eb6f548060e21a7ef
Kristof Komlossy <yiro4618@webis24.medien.uni-weimar.de>
2018-03-10 19:10:43 +0100

    *updated run.sh and README

--- dist/docker/README.md
+++ dist/docker/README.md
@@ -1,8 +1,22 @@
 1. run the follwoing command to build the image `./build.sh`
 
-2. run the follwoing command to spawn a container
+2. create a `<name>.conf` file which defines the following settings for one corpus:
+```
+#mandatory
+file_settings="<path/to/settingsfile>" #if relative, relative to the run.sh!
+data_output="<path/to/dataouput>" #if relative, relative to the run.sh!
+port="<port>"
+
+#optional
+name_container="<name>"
+data_corpus="<path/to/corpusdata>" #if relative, relative to the run.sh!
+```
+3. pass the path(s) to the configfile(s) to the run.sh:
+`./run.sh ./*.conf` or `./run.sh ./conf/someconfig.conf` or ... 
+
+<!-- 2. run the follwoing command to spawn a container
 `./run.sh -s settings_netspeak.py -d ./ -p 4000 -o docker_output`
  
-3. visit `localhost:4000/netspeak`
+3. visit `localhost:4000/netspeak` -->
 
 4. Stop and remove a container by running `docker stop <container> && docker rm <container>` or stop and remove all containers by running `sudo docker stop $(sudo docker ps -a -q) && sudo docker rm $(sudo docker ps -a -q)`
--- dist/docker/run.sh
+++ dist/docker/run.sh
@@ -1,70 +1,113 @@
 #!/bin/bash
+for config_file; do
+  source "$config_file"
 
-OPTS=$(getopt --name $(basename $0) --options s:d:o:p: --longoptions settingsfile:,output:,data:,port: -- $@)
+  if [ \( -z "$file_settings" \) -o \( -z "$data_output" \) -o \( -z "$port" \) ];then
+    echo "USAGE"
+    echo "  $0 [OPTIONS] --settings-file <file> --output <directory> --port <port> [--data <directory>]"
+    echo "WHERE"
+    echo "  --settings-file <file>"
+    echo "    Absolute path to the settings file of the corpus"
+    echo "  --output <directory>"
+    echo "    Absolute path to the output directory of the corpus viewer"
+    echo "    output are written"
+    echo "  --port <port>"
+    echo "    Port under which the container should be accessible"
+    echo "  --data <directory>"
+    echo "  Absolute path to the directory of the corpus"
+    echo "  Only needed if the corpus is not loaded yet"
+    exit 1
+  fi
 
-if [[ $? -ne 0 ]]; then
-    exit 2
-fi
-eval set -- "$OPTS"
+  file_settings=$(readlink -f -- $file_settings)
+  data_output=$(readlink -f -- $data_output)
+  data_corpus=$(readlink -f -- $data_corpus)
 
-while true;do
-  case "$1" in
-    -s|--settings-file)
-      file_settings="$2"
-      shift 2
-      ;;
-    -o|--output)
-      data_output="$2"
-      shift 2
-      ;;
-    -d|--data)
-      data_corpus="$2"
-      shift 2
-      ;;
-    -p|--port)
-      port="$2"
-      shift 2
-      ;;
-    --)
-      break
-      ;;
-  esac
+  mkdir -p $data_output
+
+  is_in_docker_group=$(groups | sed 's/ /\n/g' | grep '^docker$' | wc -l)
+
+  string_mount_data_output="--volume $data_output:/data"
+  string_mount_data_corpus="--volume $data_corpus:/data_corpus:ro"
+  string_mount_file_settings="--volume $file_settings:/settings.py:ro"
+  string_port="-p $port:80"
+
+  if ! [ -z "$name_container" ];then
+    string_name_container="--name $name_container"
+  fi
+
+  command="docker run -t -d -i $string_name_container $string_mount_data_output $string_mount_data_corpus $string_mount_file_settings $string_port corpus-viewer"
+
+  if [ $is_in_docker_group -eq 0 ];then
+    sudo $command
+  else
+    $command
+  fi
+  
 done
 
-if [ \( -z "$file_settings" \) -o \( -z "$data_output" \) -o \( -z "$port" \) ];then
-  echo "USAGE"
-  echo "  $0 [OPTIONS] --settings-file <file> --output <directory> --port <port> [--data <directory>]"
-  echo "WHERE"
-  echo "  --settings-file <file>"
-  echo "    Absolute path to the settings file of the corpus"
-  echo "  --output <directory>"
-  echo "    Absolute path to the output directory of the corpus viewer"
-  echo "    output are written"
-  echo "  --port <port>"
-  echo "    Port under which the container should be accessible"
-  echo "  --data <directory>"
-  echo "  Absolute path to the directory of the corpus"
-  echo "  Only needed if the corpus is not loaded yet"
-  exit 1
-fi
+exit 0
 
-file_settings=$(readlink -f -- $file_settings)
-data_output=$(readlink -f -- $data_output)
-data_corpus=$(readlink -f -- $data_corpus)
+# OPTS=$(getopt --name $(basename $0) --options s:d:o:p: --longoptions settingsfile:,output:,data:,port: -- $@)
 
-mkdir -p $data_output
+# if [[ $? -ne 0 ]]; then
+#     exit 2
+# fi
+# eval set -- "$OPTS"
 
-is_in_docker_group=$(groups | sed 's/ /\n/g' | grep '^docker$' | wc -l)
+# while true;do
+#   case "$1" in
+#     -s|--settings-file)
+#       file_settings="$2"
+#       shift 2
+#       ;;
+#     -o|--output)
+#       data_output="$2"
+#       shift 2
+#       ;;
+#     -d|--data)
+#       data_corpus="$2"
+#       shift 2
+#       ;;
+#     -p|--port)
+#       port="$2"
+#       shift 2
+#       ;;
+#     --)
+#       break
+#       ;;
+#   esac
+# done
 
-command="docker run -d -t -i --volume $data_output:/data --volume $data_corpus:/data_corpus:ro --volume $file_settings:/settings.py:ro -p $port:80 corpus-viewer"
-# command="docker run -d -t -i --mount type=bind,src=$data_output,dst=/data --mount type=bind,src=$data_corpus,dst=/data_corpus,readonly --mount type=bind,src=$file_settings,dst=/settings.py,readonly -p $port:80 corpus-viewer"
-if [ $is_in_docker_group -eq 0 ];then
-  sudo $command
-else
-  $command
-fi
+# if [ \( -z "$file_settings" \) -o \( -z "$data_output" \) -o \( -z "$port" \) ];then
+#   echo "USAGE"
+#   echo "  $0 [OPTIONS] --settings-file <file> --output <directory> --port <port> [--data <directory>]"
+#   echo "WHERE"
+#   echo "  --settings-file <file>"
+#   echo "    Absolute path to the settings file of the corpus"
+#   echo "  --output <directory>"
+#   echo "    Absolute path to the output directory of the corpus viewer"
+#   echo "    output are written"
+#   echo "  --port <port>"
+#   echo "    Port under which the container should be accessible"
+#   echo "  --data <directory>"
+#   echo "  Absolute path to the directory of the corpus"
+#   echo "  Only needed if the corpus is not loaded yet"
+#   exit 1
+# fi
 
-# if [ "$#" -ne 4 ]; then
-#     echo "run.sh <settings file> <corpus data> <port> <output data>"
+# file_settings=$(readlink -f -- $file_settings)
+# data_output=$(readlink -f -- $data_output)
+# data_corpus=$(readlink -f -- $data_corpus)
+
+# mkdir -p $data_output
+
+# is_in_docker_group=$(groups | sed 's/ /\n/g' | grep '^docker$' | wc -l)
+
+# command="docker run -t -d -i --volume $data_output:/data --volume $data_corpus:/data_corpus:ro --volume $file_settings:/settings.py:ro -p $port:80 corpus-viewer"
+# # command="docker run -d -t -i --mount type=bind,src=$data_output,dst=/data --mount type=bind,src=$data_corpus,dst=/data_corpus,readonly --mount type=bind,src=$file_settings,dst=/settings.py,readonly -p $port:80 corpus-viewer"
+# if [ $is_in_docker_group -eq 0 ];then
+#   sudo $command
 # else
+#   $command
 # fi
