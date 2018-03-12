ef9e6dcecb20df610c04c8dabcd8a24b7c0a680e
Kristof Komlossy <yiro4618@webis24.medien.uni-weimar.de>
2018-03-12 11:42:33 +0100

    -disabled mutli port middleware

--- viewer-framework/viewer-framework/middleware.py
+++ viewer-framework/viewer-framework/middleware.py
@@ -30,17 +30,17 @@ class SqlPrintMiddleware(object):
         return response
 
 
-from django.conf import settings
+# from django.conf import settings
 
-class MultiPortMiddleware(object):
-    def __init__(self, get_response):
-        self.get_response = get_response
-    """
-    Middleware changes the SESSION_COOKIE_NAME to use the current port in the name
-    """
+# class MultiPortMiddleware(object):
+#     def __init__(self, get_response):
+#         self.get_response = get_response
+#     """
+#     Middleware changes the SESSION_COOKIE_NAME to use the current port in the name
+#     """
 
-    def __call__(self, request):
-        response = self.get_response(request)
-        settings.SESSION_COOKIE_NAME = 'sessionid_{}'.format(request.META['SERVER_PORT'])
-        settings.CSRF_COOKIE_NAME = 'csrftoken_{}'.format(request.META['SERVER_PORT'])
-        return response
+#     def __call__(self, request):
+#         response = self.get_response(request)
+#         settings.SESSION_COOKIE_NAME = 'sessionid_{}'.format(request.META['SERVER_PORT'])
+#         settings.CSRF_COOKIE_NAME = 'csrftoken_{}'.format(request.META['SERVER_PORT'])
+#         return response
--- viewer-framework/viewer-framework/settings.py
+++ viewer-framework/viewer-framework/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'viewer-framework.middleware.MultiPortMiddleware',
+    # 'viewer-framework.middleware.MultiPortMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
