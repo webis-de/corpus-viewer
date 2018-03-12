8683eb95737716eeb8c520b9a63ebe9687bb302d
Kristof Komlossy <yiro4618@webis24.medien.uni-weimar.de>
2018-03-12 10:00:06 +0100

    +port specific session names

--- viewer-framework/viewer-framework/middleware.py
+++ viewer-framework/viewer-framework/middleware.py
@@ -28,3 +28,18 @@ class SqlPrintMiddleware(object):
         print('___________________________________________________________')
 
         return response
+
+
+from django.conf import settings
+
+class MultiPortMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+    """
+    Middleware changes the SESSION_COOKIE_NAME to use the current port in the name
+    """
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        settings.SESSION_COOKIE_NAME = 'sessionid_{}'.format(request.META['SERVER_PORT'])
+        return response
