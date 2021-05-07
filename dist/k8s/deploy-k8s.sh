c7285dd0b3464bb6b38b1e25cd51c481d62d3ae9
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2021-05-07 14:24:41 +0200

    moved yamls to code-admin-knowledge-base

--- dist/k8s/deploy-k8s.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-kubectl apply --namespace webisservices -f dist/k8s/pan18-author-profiling-k8s.yml
-kubectl apply --namespace webisservices -f dist/k8s/pan18-style-k8s.yml
-kubectl apply --namespace webisservices -f dist/k8s/pan18-masking-k8s.yml
-kubectl apply --namespace webisservices -f dist/k8s/pan18-aic-k8s.yml
-
